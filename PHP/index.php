f3ae52a23dfd15504129b6c0ce6d4f193f71a464
JohnBDev <63297273+JohnB17@users.noreply.github.com>
2024-11-13 10:36:01 -0500

    The UI is starting.

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,44 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Spotify Extended History Stats</title>
-    </head>
-    <body>
-        <h1>Spotify Extended History Stats</h1>
-    </body>
+
+<head>
+    <title>Spotify Extended History Stats</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="text-bg-dark">
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="grid-container">
+            <div class="card bg-dark text-white" style="width: 20rem;">
+                <div class="card-body">
+                    <h5 class="card-title card-title-custom">Spotify Extended History Stats</h5>
+                    <h6 class="card-subtitle mb-2 card-subtitle-custom">&copy; JohnBDev</h6>
+                </div>
+            </div>
+            <div class="card bg-dark text-white" style="width: 20rem;">
+                <div class="card-body">
+                    <h5 class="card-title card-title-custom">Total</h5>
+                    <p class="card-text mb-2">
+                        <strong>Total Songs: </strong><span id="totalListened"></span>
+                        <strong></strong>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="card bg-dark text-white" style="width: 20rem;">
+            <div class="card-body">
+                <h5 class="card-title card-title-custom">Spotify Extended History Stats</h5>
+                <h6 class="card-subtitle mb-2 card-subtitle-custom">&copy; JohnBDev</h6>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
 </html>
