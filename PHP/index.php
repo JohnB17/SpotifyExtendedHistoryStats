01e27a6283144c1bf400cd2d81543d54eaa5b8f5
JohnBDev <63297273+JohnB17@users.noreply.github.com>
2024-11-13 09:49:42 -0500

    Started HTML and gitignored my spotify data

---
+++ PHP/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Spotify Extended History Stats</title>
+    </head>
+    <body>
+        <h1>Spotify Extended History Stats</h1>
+    </body>
+</html>
